64531967bb107f72be5c6f44b9cd8a68d04b4120
Ivana Yovcheva <iyovcheva@vmware.com>
2018-08-24 13:41:26 +0300

    Add url query
    Signed-off-by: Ivana Yovcheva <iyovcheva@vmware.com>

--- guestbook/index.rb
+++ guestbook/index.rb
@@ -26,3 +26,13 @@ post "/sign" do
   
     erb :signed, locals: { visitors: visitors}
 end
+
+get '/signatures' do
+    # matches "GET /signatures?guest=foo%20bar&firstname=foo&lastname=bar"
+
+    if visitors.include?(params[:guest])
+        "Visitor " + params[:firstname] + " " + params[:lastname] + " has signed the Guestbook."
+    else
+        "Visitor " + params[:firstname] + " " + params[:lastname] + " has not signed the Guestbook."
+    end
+end
